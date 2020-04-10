5e4b87b46f9873f5eed774131460abd5da7dfec7
Laragervaise <32794879+Laragervaise@users.noreply.github.com>
2020-04-10 14:19:16 +0200

    Initialize this project

---
+++ Assets/MyNewClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNewClass : MonoBehaviour {
+
+	/* ... */
+
+	void Start() { /* ... */ }
+
+	void Update() {
+		if( OVRInput.Get( OVRInput.Button.One ) ) Debug.LogWarning( "Button is pressed" );
+	}
+
+	/* ... */
+}
