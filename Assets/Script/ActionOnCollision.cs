0cf952f9296a7e945040144e0a89cdd7fc38fc80
Laragervaise <32794879+Laragervaise@users.noreply.github.com>
2020-04-28 01:52:35 +0200

    break glass + button and door
    - break glass by throwing green cubes hard enough on the glass
    - push the red square button (on the wall) hard enough by throwing anything on it to open the little door

--- Assets/Script/ActionOnCollision.cs
+++ Assets/Script/ActionOnCollision.cs
@@ -7,9 +7,9 @@ public class ActionOnCollision : MonoBehaviour {
     private float threshold = 8;
 
     void OnCollisionEnter(Collision collision) {
-        // if object thrown hard enough on a breakable object, the breakable obkect disappear
+        // if object thrown hard enough on a tagged object, this tagged object disappears
         if (collision.relativeVelocity.magnitude >= threshold && collision.gameObject.CompareTag("glass")) {
-            Destroy(collision.gameObject);
+            Destroy(collision.gameObject); // or trigger whatever action we need (like opening a door by destroying another gameObject)
         }
     }
 }
