b621d0bca910b2077776f73d30b976374bb9a30d
Laragervaise <32794879+Laragervaise@users.noreply.github.com>
2020-05-11 16:12:23 +0200

    tuto trigger

---
+++ Assets/Script/PlayTuto.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTuto : MonoBehaviour
+{
+
+    public AudioClip SoundToPlay;
+    [Range(0, 5)]
+    public float Volume = 0.5f;
+    [Range(0, 5)]
+    public float duration = 3f;
+    AudioSource audio;
+    private bool alreadyPlayed = false;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        } 
+        else if (OVRInput.Get(OVRInput.Button.One))
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            StartCoroutine(Break(duration));
+        }
+    }
+
+    IEnumerator Break(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+    }
+
+}
